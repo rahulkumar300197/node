2111d3290fbf035cafd1794f7e6c2d3dc95fd731
rahulkumar300197 <rahulkumar300197>
2017-10-25 15:21:01 +0530

    Initial Commit

---
+++ a.js
@@ -0,0 +1,23 @@
+/*var abc ={a:"string",b:"string"};
+
+function_name(abc)
+ console.log(abc);
+
+
+
+function function_name(argument)
+{
+  argument={a:"dgadh",b:"63"}
+}  
+
+*/
+
+var abc =[{a:"Rahul",b:"Kumar"},"fwg"];
+ console.log(abc[0].a);
+ console.log(abc[0].b);
+console.log(abc[1]);
+console.log(process.argv);
+var x={"aba":"string","veve":"veE"};
+x.toString();
+console.log(x.hasOwnProperty("aba"));
+
